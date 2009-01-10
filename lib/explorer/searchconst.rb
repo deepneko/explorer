9fcc9dc7ccd267fb40964998707fe7088bc53cdd
koko_master <koko_master@76beb010-293a-44e9-8231-3d3008485057>
2009-01-10 15:33:12 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/explorer@106 76beb010-293a-44e9-8231-3d3008485057

--- lib/explorer/searchconst.rb
+++ lib/explorer/searchconst.rb
@@ -1,7 +1,6 @@
 module Explorer
   class SearchConst
     attr_accessor :SEARCH_DIR
-    attr_accessor :URL_PATH
     attr_accessor :CGI_PATH
     attr_accessor :DIR_PATH
     attr_accessor :ICON_PATH
@@ -11,17 +10,14 @@ module Explorer
     attr_accessor :LISTNEW_SIZE
     
     def initialize
-      #@SEARCH_DIR = "/home/deepneko/tmp/"
-      #@URL_PATH = 'http://deepneko.dyndns.org/kokotech/archives/'
-      @SEARCH_DIR = "/Users/deepneko/study/"
-      @URL_PATH = 'http://localhost/'
       @CGI_PATH = './explorer.cgi'
-      #@DIR_PATH = '/home/deepneko/tmp/'
-      @DIR_PATH = '/Users/deepneko/study/'
       @ICON_PATH = '../img/'
       @DIR_ICON = @ICON_PATH + 'dir.gif'
       @FILE_ICON = @ICON_PATH + 'text.gif'
-      @DB = "allfile.db"
+
+      # need configuration
+      @SEARCH_DIR = '/home/deepneko/tmp/'
+      @DB = '/home/deepneko/allfile.db'
       @LISTNEW_SIZE = 10
     end
   end
