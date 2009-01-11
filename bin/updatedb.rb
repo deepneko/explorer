24896d9ef428d34a5263c4236240d3747ee336fa
katsuma <katsuma@76beb010-293a-44e9-8231-3d3008485057>
2009-01-11 10:06:39 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/explorer@114 76beb010-293a-44e9-8231-3d3008485057

--- bin/updatedb.rb
+++ bin/updatedb.rb
@@ -13,6 +13,7 @@ alldate = $con.execute("select date from filelist").flatten
 # if file doesn't exist, delete from database
 allpath.each do |path|
   if !File.exists?(path)
+    print "delete:" + path + "\n"
     $con.execute("delete from filelist where path='#{path}'")
   end
 end
@@ -20,12 +21,14 @@ end
 # update or insert
 Explorer.allfile.each do |path|
   date = File.mtime(path).strftime('%Y-%m-%d %H:%M:%S')
+  print date + " " + path + "\n"
   if i = allpath.index(path)
-    print date + " " + path + "\n"
     if alldate[i] != date
+      print "update:" + date + " " + path + "\n"
       $con.execute("update filelist set date='#{date}' where path='#{path}'")
     end
   else
+    print "insert:" + date + " " + path + "\n"
     $con.execute("insert into filelist(path, date) values('#{path}', '#{date}')")
   end
 end
