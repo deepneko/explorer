fc448511977f14a30a88bb3b44f262090c8ee3f4
katsuma <katsuma@76beb010-293a-44e9-8231-3d3008485057>
2009-03-08 04:17:22 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/explorer@173 76beb010-293a-44e9-8231-3d3008485057

--- bin/encode.rb
+++ bin/encode.rb
@@ -36,7 +36,7 @@ end
 # ffmpeg encode at remote host
 # scp flv remote2local
 encodelist.each do |path|
-  path.gsub!(/ /, "\ ")
+  path.gsub!(/ /, "\\ ")
   file = File.basename(path)
   p "Filename:" + file
   command = "scp -P #{$const.SSH_PORT} #{path} tomoyo@deepneko.dyndns.org:~/ffmpeg/#{file}"
