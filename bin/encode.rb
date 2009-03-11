a1959e402fb2858c5d89e2b24e646a0d166569e1
katsuma <katsuma@76beb010-293a-44e9-8231-3d3008485057>
2009-03-11 10:43:47 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/explorer@237 76beb010-293a-44e9-8231-3d3008485057

--- bin/encode.rb
+++ bin/encode.rb
@@ -34,6 +34,8 @@ elsif getopt[:f]
   encodelist = $con.execute("select path, flv from filelist where path like '%#{getopt[:f]}'" + movie_option).flatten
 end
 
+p encodelist
+
 # 1. scp avi,wmv,mpg local2remote
 # 2. ffmpeg encode at remote host
 # 3. scp flv remote2local
