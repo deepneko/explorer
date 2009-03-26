a1f25e4f2277b5b6a3b56a0caea1303b9fecbad9
katsuma <katsuma@76beb010-293a-44e9-8231-3d3008485057>
2009-03-26 08:45:56 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/explorer@244 76beb010-293a-44e9-8231-3d3008485057

--- bin/encode.rb
+++ bin/encode.rb
@@ -10,17 +10,34 @@ getopt = Hash.new
 begin
   OptionParser.new do |opt|
     opt.on('-a') {|v| getopt[:a] = v }
+    opt.on('-u') {|v| getopt[:u] = v }
     opt.on('-d VALUE') {|v| getopt[:d] = v }
     opt.on('-f VALUE') {|v| getopt[:f] = v }
     opt.parse!(ARGV)
   end
 rescue
-  p "usage: ./encode.rb [-a | -d directory | -f file]"
+  p "usage: ./encode.rb [-a | -u | -d directory | -f file]"
   exit! 0
 end
 
 # movie only
 movie_option = " and (path like '%.avi' or path like '%.wmv')"
+
+# if flv size = 0 then flv data delete from db
+if getopt[:u]
+  #encodelist = $con.execute("select path, flv from filelist")
+  #encodelist.each do |path, flv|
+  #  if File.stat($enconst.FLV_DIRECTORY + flv).size <= 0
+  #    $con.execute("update filelist set flv='' where flv='#{flv}'")
+  #  end
+  #end
+
+  Dir.glob($enconst.FLV_DIRECTORY + "*.flv").each do |file|
+    p file
+  end
+
+  exit! 0
+end
 
 # select alldata from table
 if getopt[:a]
@@ -32,35 +49,31 @@ elsif getopt[:f]
 end
 
 $enconst.ENCODE_SERVER.each do |host, port|
-  t = Thread.new do 
-    encodelist.each do |path, flv|
-      src = File.basename(path)
-      dist = Digest::MD5.new.update(src).to_s + ".flv"
+  encodelist.each do |path, flv|
+    src = File.basename(path)
+    dist = Digest::MD5.new.update(src).to_s + ".flv"
 
-      p dist
-      if flv != dist
-        # 1. scp avi,wmv,mpg local2remote
-        # 2. ffmpeg encode at remote host
-        # 3. scp flv remote2local
-        # 4. rm all tmp file
-        scp_up = "scp -P #{port} \"#{path}\" #{host}:~/"
-        #encode = "ssh -p #{port} #{host} '" + Encoder::ffmpeg(src, dist) + "'"
-        #scp_down = "scp -P #{port} #{host}:~/#{dist} #{$enconst.FLV_DIRECTORY}"
-        #rm = "ssh -p #{port} #{host} 'rm -f *.avi;rm -f *.AVI;rm -f *.flv;rm -f *.wmv'"
-
-        # exec command
-        `#{scp_up}`
-        #`#{encode}`
-        #`#{scp_down}`
-        #`#{rm}`
-
-        begin
-          #$con.execute("update filelist set flv='#{dist}' where path=\"#{path}\"")
-        rescue SQLite3::SQLException
-          p "Exception:" + dist + ":" + path + "\n"
-        end
+    if flv != dist
+      # 1. scp avi,wmv,mpg local2remote
+      # 2. ffmpeg encode at remote host
+      # 3. scp flv remote2local
+      # 4. rm all tmp file
+      scp_up = "scp -P #{port} \"#{path}\" #{host}:~/"
+      #encode = "ssh -p #{port} #{host} '" + Encoder::ffmpeg(src, dist) + "'"
+      #scp_down = "scp -P #{port} #{host}:~/#{dist} #{$enconst.FLV_DIRECTORY}"
+      #rm = "ssh -p #{port} #{host} 'rm -f *.avi;rm -f *.AVI;rm -f *.flv;rm -f *.wmv'"
+      
+      # exec command
+      `#{scp_up}`
+      #`#{encode}`
+      #`#{scp_down}`
+      `#{rm}`
+      
+      begin
+        #$con.execute("update filelist set flv='#{dist}' where path=\"#{path}\"")
+      rescue SQLite3::SQLException
+        p "Exception:" + dist + ":" + path + "\n"
       end
     end
   end
-  t.join
 end
