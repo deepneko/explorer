3d0f8a717a8aad3dafdd624c6b147bfefd3d722b
koko_master <koko_master@76beb010-293a-44e9-8231-3d3008485057>
2009-06-21 02:38:13 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/explorer@519 76beb010-293a-44e9-8231-3d3008485057

--- lib/encode/encodeconst.rb
+++ lib/encode/encodeconst.rb
@@ -1,14 +1,10 @@
 module Encoder
   class EncoderConst
     attr_accessor :FLV_DIRECTORY
-    attr_accessor :ENCODE_SERVER
-    attr_accessor :ENCODE_PORT
     attr_accessor :ENCODE_DIRECTORY
     
     def initialize
       @FLV_DIRECTORY = "/usr/home/BACKUP/kotachu/flv/"
-      @ENCODE_SERVER = "tomoyo@deepneko.dyndns.org"
-      @ENCODE_PORT = 20022
       @ENCODE_DIRECTORY = "/usr/home/BACKUP/kotachu/Movie/\[\ Anime\ \]/"
     end
   end
